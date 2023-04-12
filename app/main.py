1ed6bfb6d4bb26c7985c6c2361494732807fad58
harishtpj <harishtpj@hotmail.com>
2023-04-12 19:51:17 +0530

    Added support for reading no of tables in db

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 import sys
-
 from dataclasses import dataclass
 
 # import sqlparse - available if you need it!
 
-database_file_path = sys.argv[1]
-command = sys.argv[2]
+def handleCmd(dbpath, cmd):
+    if cmd == ".dbinfo":
+        db_info = {}
+        with open(dbpath, "rb") as dbfile:
+            dbfile.seek(16)
+            db_info["database page size"] = int.from_bytes(dbfile.read(2), byteorder="big")
+            
+            dbfile.seek(103)
+            db_info["number of tables"] = int.from_bytes(dbfile.read(2), byteorder="big")
 
-if command == ".dbinfo":
-    with open(database_file_path, "rb") as database_file:
-        database_file.seek(16)
-        page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        print(f"database page size: {page_size}")
-else:
-    print(f"Invalid command: {command}")
+            for info, val in db_info.items():
+                print(f"{info}: {val}")
+
+    else:
+        sys.stderr.write(f"Invalid command: {command}\n")
+
+
+if __name__ == "__main__":
+    db_path = sys.argv[1]
+    cmd = sys.argv[2]
+
+    handleCmd(db_path, cmd)
