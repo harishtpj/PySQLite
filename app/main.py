a0e2c3397c3c2633163d54c27d3e84799976e4a6
harishtpj <harishtpj@hotmail.com>
2023-04-12 19:36:41 +0530

    Added support for dbinfo cmd

--- app/main.py
+++ app/main.py
@@ -9,12 +9,8 @@ command = sys.argv[2]
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
-
-        # Uncomment this to pass the first stage
-        # database_file.seek(16)  # Skip the first 16 bytes of the header
-        # page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        # print(f"database page size: {page_size}")
+        database_file.seek(16)
+        page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        print(f"database page size: {page_size}")
 else:
     print(f"Invalid command: {command}")
